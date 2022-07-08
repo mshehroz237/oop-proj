261d6b4b7f6b49bd9c511dd5173ef8537196a600
Muhammad <muhamadshehroz10089@icloud.com>
2022-07-07 23:14:19 -0400

    added promots

--- index.js
+++ index.js
@@ -1,46 +1,141 @@
 //What are we going to do in inquirer
 const Manager = require('./lib/Manager')
-const Employee = require('./lib/Employee');
 const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+// const pageRender = require('./src/template')
+const fs = require('fs');
 const Engineer = require('./lib/Engineer');
-const inquirer = require("inquirer")
 
-const pageRender = require('./src/template')
 //When we start the applications, ther should be a a prompt for a user to create a manager
 let teamMembers = []
-inquirer.prompt([{
-    type: 'input',
-    name: 'name',
-    message: 'What is managers name'
-},
-{
+function managerPrompt() {
+    inquirer.prompt({
+        type: 'input',
+        name: 'name',
+        message: 'What is managers name'
+    },
+        {
 
-    type: 'input',
-    name: 'id',
-    message: 'What is your employee id'
-},
-{
-    type: 'input',
-    name: 'email',
-    message: 'What is your project title'
-},
-{
-    type: 'input',
-    name: 'officeNumber',
-    message: 'What is your project title'
+            type: 'input',
+            name: 'id',
+            message: 'What is your employee id'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your project title'
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is your project title'
+        }
+    )
+        .then(answers => {
+
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+            teamMembers.push(manager)
+
+        })
+}
+function prompts() {
+    inquirer.prompt({
+        type: 'list',
+        name: 'prompts',
+        message: 'Would you like to add anything else?',
+        choices: [
+            'Add an Intern',
+            'Add an engineer',
+            'Are you done?'
+        ]
+    })
+        .then(answers => {
+            if (answers.prompts === 'Add an Intern') {
+                intern()
+            }
+            if (answers.prompts === 'Add an engineer') {
+                enginnerQuestion()
+            }
+            if (answers.prompts === 'Are you done?') {
+                return;
+            }
+        })
+
+}
+function enginnerQuestion() {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is enginner name'
+    },
+    {
+
+        type: 'input',
+        name: 'id',
+        message: 'What is engineer id '
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is enginner email'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your enginers github'
+    }
+    ]
+    )
+        .then(answers => {
+
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+            teamMembers.push(Engineer)
+
+        })
+        prompts();
 }
 
-])
-.then(answers => {
+function intern() {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is interns name'
+    },
+    {
 
-    const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
-    teamMembers.push(manager)
-})
+        type: 'input',
+        name: 'id',
+        message: 'What is interns id '
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is interns email'
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What is interns school'
+    }
+    ]
+    )
+        .then(answers => {
 
-// Do the same thing for engineer and intern
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
+            teamMembers.push(intern)
+
+        })
+        prompts();
+}
+
+managerPrompt();
+
+prompts();
+
+
 
 
 //Once we created all our team members we want to generate an html file based on the ccontetn the user put in
 
 // This is the data we are passing into the file creation
-pageRender(teamMembers)
+// pageRender(teamMembers);
--- src/template.js
+++ src/template.js
@@ -1,35 +1,38 @@
-const Employee = require("../lib/Employee")
+// const Employee = require("../lib/Employee")
 
-const generateTemplate = team => {
-    const generateManager = manager => {
-        return `<div>`
-    }
-    const generateEngineer = engineer => {
-        return `<div>`
-    }
-    const generateIntern = intern => {
-        return `<div>`
-    }
- const html = []
- html.push(team.filter(Employee.getRole === 'Manager').map(manager => generateManager(manager)))
+// const generateTemplate = team => {
+//     const generateManager = manager => {
+//         return `<div>`
+//     }
+//     const generateEngineer = engineer => {
+//         return `<div>`
+//     }
+//     const generateIntern = intern => {
+//         return `<div>`
+//     }
+//  const html = []
+//  html.push(team.filter(Employee.getRole === 'Manager').map(manager => generateManager(manager)))
 
- html.push(team.filter(Employee.getRole === 'Engineer').map(Engineer => generateEngineer(Engineer)).join(''))
+//  html.push(team.filter(Employee.getRole === 'Engineer').map(Engineer => generateEngineer(Engineer)).join(''))
 
- html.push(team.filter(Employee.getRole === 'Intern').map(Intern => generateIntern(Intern)).join(''))
+//  html.push(team.filter(Employee.getRole === 'Intern').map(Intern => generateIntern(Intern)).join(''))
 
- return html.join('')
+//  return html.join('')
 
-}
+// }
+// function createfile = createfile =>{
+//     fs.writeFile('index.html', template.js)
+// }
+// module.exports = generateTemplate;
+// // module.exports  = team => {
+// //     return `
+// //     <!DOCTYPE html>
 
-module.exports  = team => {
-    return `
-    <!DOCTYPE html>
 
 
 
 
-
-    ${generateTemplate(team)}
+// //     ${generateTemplate(team)}
     
-    `
-}
+// //     `
+// // }
